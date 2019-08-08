63a6caf9bb994fc4a23f0be4253067271bc6a82e
poopoomarvin <hchs010077@gmail.com>
2019-08-08 20:02:43 +0800

    data

--- uniq_location_log.py
+++ uniq_location_log.py
@@ -13,17 +13,20 @@ while line:
     line = line.replace("\n", "")
     spot_map[line] = True
     line = spot_file.readline()
-
+line = log_file.readline()
 line = log_file.readline()
 while line:
     line = line.replace("\n", "")
+    print("uid:" + line.split(",")[1])
     if uid != line.split(",")[1]:
+        uid = line.split(",")[1]
         user_visited_location.clear()
     location = line.split(",")[4]
     if location == "":
         report_including_ALL.write(line + "\n")
     elif (location in user_visited_location) == False:
         user_visited_location[location] = True
+        print(str(uid) + ", " + location)
         report_including_ALL.write(line + "\n")
         if location in spot_map:
             report_including_ONLYPOI.write(line + "\n")
