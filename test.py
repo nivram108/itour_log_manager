918c687877ad1b5876db884c687cd65e548a0dc2
poopoomarvin <hchs010077@gmail.com>
2019-08-08 23:59:29 +0800

    view fami

---
+++ test.py
@@ -0,0 +1,1 @@
+f = open(fami_spot
--- view_fami.py
+++ view_fami.py
@@ -3,7 +3,7 @@ fami_spot = open("fami_spot.txt", "r")
 viewed_checkin = open("viewed_community.csv", "r")
 view_inclusive = open("view_inclusive.csv", "w")
 view_exclusive = open("view_exclusive.csv", "w")
-spots = fami_spot.readline().split(",").replace("\n", "")
+spots = fami_spot.readline().replace("\n", "").split(",")
 uid_fami_map_inclusive = {}
 uid_fami_map_exclusive = {}
 
@@ -12,16 +12,18 @@ while line:
     l = line.split(",")
     uid = l[1]
     if uid == "INVALID":
-        continue
+        line = line
     elif "_" in uid == False:
         for x in range(len(spots)):
             key = uid + spots[x]
             uid_fami_map_exclusive[key] = l[x + 2]
             uid_fami_map_inclusive[key] = l[x + 2]
+            print("EX:" + key)
     else:
         for x in range(len(spots)):
             key = uid + spots[x]
             uid_fami_map_inclusive[key] = l[x + 2]
+            print("IN:" + key)
     line = fami_uid.readline().replace("\n", "")
 
 line = viewed_checkin.readline().replace("\n", "")
