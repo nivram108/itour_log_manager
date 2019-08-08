0982b09d1ffcaa73edf30319526f0a8c6ee5ac79
poopoomarvin <poopoomarvin@github.com>
2019-08-08 23:15:46 +0800

    fami

---
+++ fami.py
@@ -0,0 +1,26 @@
+name_uid_file = open("name_uid.csv", "r")
+fami_file = open("fami.csv", "r")
+fami_uid_output = open("fami_uid.csv", "w")
+
+uid_map = {}
+line = name_uid_file.readline()
+while line:
+    line = line.replace("\n", "")
+    l = line.split(",")
+    uid_map[l[1]] = l[0]
+    line = uid_file.readline()
+
+line = fami_file.readline()
+
+while line:
+    line = line.replace("\n", "")
+    l = line.split(",")
+    uid = ""
+    if l[0] in uid_map:
+        uid = uid_map[l[0]]
+    else if l[1] in uid_map:
+        uid = uid_map[l[0]]
+    else:
+        uid = "INVALID"
+    fami_uid_output.write(line.replace(l[1], uid))
+    line = fami_file.readline()
